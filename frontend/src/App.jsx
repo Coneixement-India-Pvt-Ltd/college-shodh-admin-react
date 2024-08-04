96c203f1614d6d964c8de8372543775667a3c7c5
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2024-08-04 16:19:58 +0530

    feat: auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Signup from './components/Auth/Signup/Signup';
+import Login from './components/Auth/Login/Login';
+import Home from './components/Dashboard/Home';
+import ForgotPassword from './components/Auth/ForgotPassword/ForgotPassword';
+import ResetPassword from './components/Auth/ForgotPassword/ResetPassword';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world from Admin!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/resetPassword/:token" element={<ResetPassword />} />
+        
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Dashboard/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='font-medium text-2xl text-orange-600'>Hello from Home Page - Admin Dashboard</div>
+  )
+}
+
+export default Home
