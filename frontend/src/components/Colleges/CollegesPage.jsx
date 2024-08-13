95c2dc461d8184f8f59e202e9bdd89825aced7b6
Om Singh <86137170+Omprakashx@users.noreply.github.com>
2024-08-13 15:46:21 +0530

    Admin Folder Structure

---
+++ frontend/src/components/Colleges/CollegesPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '../Admin_panel/Sidebar';
+import Navbar from '../Admin_panel/Navbar';
+import Card from './Card';
+
+
+const CollegesPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+      <Sidebar />
+      
+    </div>
+  )
+}
+
+export default CollegesPage;
