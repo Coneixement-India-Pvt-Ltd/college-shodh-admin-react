4aa89dfdcd640cf9ce269df895f58e8de2b351d6
Om Singh <86137170+Omprakashx@users.noreply.github.com>
2024-08-14 16:05:56 +0530

    Dropdown Menu fixed

--- frontend/src/components/Admin_panel/Sidebar.jsx
+++ frontend/src/components/Admin_panel/Sidebar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -42,8 +41,8 @@ const Sidebar = () => {
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
             <li>
-              <Link 
-              to="/dashboard"
+              <Link
+                to="/dashboard"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
