fdfd27b26bba458071f0b7e3125b91992a9333ee
Om Singh <86137170+Omprakashx@users.noreply.github.com>
2024-08-20 17:57:20 +0530

    College Page API

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,46 @@
-import express from 'express'
-const app = express()
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
-dotenv.config()
-import { UserRouter } from './routes/user.js';
-import cookieParser from 'cookie-parser'
+dotenv.config();
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import { UserRouter } from "./routes/user.js";
+import cookieParser from "cookie-parser";
+import College from "./models/courseSchema.js"
 
-app.use(express.json())
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
-app.use('/auth', UserRouter)
-app.use(cookieParser())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use("/auth", UserRouter);
+app.use(cookieParser());
 
-mongoose.connect(process.env.ATLASDB_URL)
-.then(() => {
-  console.log('MongoDB connected');
-})
-.catch(err => {
-  console.error('MongoDB connection error:', err);
+mongoose
+  .connect(process.env.ATLASDB_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.get("/api/courses", async (req, res) => {
+  try {
+    const listings = await College.find();
+    // console.log(listings);
+    res.status(200).json(listings);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
-})
-
-
-
-
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
--- frontend/src/components/Auth/Signup/Signup.jsx
+++ frontend/src/components/Auth/Signup/Signup.jsx
@@ -11,29 +11,29 @@ const Signup = () => {
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    Axios.post("http://localhost:3000/auth/signup", {
-      username,
-      email,
-      password,
-    })
-      .then((response) => {
-        if (response.data.status) {
-          alert("Signup successful. Please login to continue.");
-          console.log(response);
-          navigate("/login");
-        } 
-        else {
-          // setError("Something went wrong. Please try again.");
-          console.log(response.data);
-        }
-      })
-      .catch((error) => {
-        console.log(error.response.data);
-        setError("An error occurred. Please try again."); 
-      });      
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   Axios.post("http://localhost:3000/auth/signup", {
+  //     username,
+  //     email,
+  //     password,
+  //   })
+  //     .then((response) => {
+  //       if (response.data.status) {
+  //         alert("Signup successful. Please login to continue.");
+  //         console.log(response);
+  //         navigate("/login");
+  //       } 
+  //       else {
+  //         // setError("Something went wrong. Please try again.");
+  //         console.log(response.data);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error.response.data);
+  //       setError("An error occurred. Please try again."); 
+  //     });      
+  // };
 
   return (
     <section className="bg-white">
