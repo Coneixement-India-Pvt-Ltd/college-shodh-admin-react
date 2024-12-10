f2b9fdccc7c32d06b29bd99ae31976dad981a749
sammy429b <samarthnbahirgonde49@gmail.com>
2024-12-10 16:15:43 +0530

    pages moved from component directory to pages directory

---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import DashboardHome from "../components/Dashboard/DashboardHome";
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/dashboard/",
+          {
+            withCredentials: true, // Send cookies with the request
+          }
+        );
+
+        if (response.data.isAuthenticated) {
+          setIsAuthenticated(true);
+        } else {
+         // navigate("/login"); // Redirect to login if not authenticated
+        }
+      } catch (error) {
+        console.error("Authentication check failed:", error);
+        navigate("/login"); // Redirect to login if there's an error
+      }
+    };
+
+    checkAuthentication();
+  }, [navigate]);
+
+  if (!isAuthenticated) {
+    return <p>Loading...</p>; // Optionally render a loading state while checking auth
+  }
+
+  return (
+    <>
+      {/* <Navbar />
+      <Sidebar /> */}
+      <DashboardHome />
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Add from '../components/Create_form/Add'
+
+const CreatePage = () => {
+  return (
+    <>
+    {/* <Navbar />
+    <Sidebar /> */}
+    {/* <Create /> */}
+    <Add />
+    </>
+  )
+}
+
+export default CreatePage
